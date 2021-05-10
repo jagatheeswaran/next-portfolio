ef53c33468e684b17167c020a7a3ca2ff989500a
jagatheeswaran <jagatheeswaran.t.s@gmail.com>
2021-05-10 22:11:00 +0530

    next concepts - link, style, NRouter, NProgress

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+const About = () => (
+  <Layout title="About">
+    <p>A Javascript Programmer</p>
+    <img src="/static/js-logo.png" alt="JavaScript" height="200px" />
+  </Layout>
+);
+
+export default About;
---
+++ pages/hireme.js
@@ -0,0 +1,14 @@
+import Layout from "../components/Layout";
+
+const HireMe = () => (
+  <Layout title="Hire me">
+    <p>
+      You can hire me at{" "}
+      <a href="mailto:jagatheeswaran.t.s@gmail.com">
+        jagatheeswaran.t.s@gmail.com
+      </a>
+    </p>
+  </Layout>
+);
+
+export default HireMe;
