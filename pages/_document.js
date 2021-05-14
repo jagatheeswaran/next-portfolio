27fc05b6788dcc430c5024220fc1b77b2a5d3c40
jagatheeswaran <jagatheeswaran.t.s@gmail.com>
2021-05-14 20:46:56 +0530

    final commit

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,10 @@ export default class MyDocument extends Document {
           />
           <meta charSet="utf-8" />
           <meta name="robots" content="noindex, nofollow" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
